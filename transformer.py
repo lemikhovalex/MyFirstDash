dae0e04638290cbf5f6b4730b4bccca47a804819
Lemikhov Alex <lemikhovalex@gmail.com>
2020-09-09 15:38:51 +0300

    now there is not id transformr

--- transformer.py
+++ transformer.py
@@ -1,5 +1,4 @@
 def transform(df):
-    # out = df.copy()
-    # out['y'] = out['x'] ** 2 + 2 * out['x'] * out['y'] + out['y'] ** 2
-    # return out
-    return df
+    out = df.copy()
+    out['y'] = out['x'] ** 2 + 2 * out['x'] * out['y'] + out['y'] ** 2
+    return out
